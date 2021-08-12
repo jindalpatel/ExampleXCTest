d5370aca4ebcd46d1fca92990970f33f9fcf4685
Zindal <jindalp@splunk.com>
2021-08-12 14:07:44 +0530

    //

---
+++ TSLogEvent-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  TSLogEvent-Bridging-Header.h
+//  TSLogEvent
+//
+//  Created by Jindal Patel on 12/08/21.
+//  Copyright © 2021 Title Source. All rights reserved.
+//
+
+#import "SplunkMint.h"
